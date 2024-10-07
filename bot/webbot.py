97a212c9ca2d04d21099087e98e7abc9fd65429f
Tinsae Alako <tinsae.alako@kegeberewtech.com>
2024-10-07 11:45:13 +0300

    bot

--- bot/webbot.py
+++ bot/webbot.py
@@ -37,6 +37,7 @@ DEPOSIT_AMOUNT = range(1)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
+        [InlineKeyboardButton("Register", callback_data='register')], 
         [InlineKeyboardButton("Check Balance", callback_data='check_balance'),
          InlineKeyboardButton("Deposit", callback_data='deposit')],
         [InlineKeyboardButton("Contact Support", callback_data='contact_support'),
@@ -58,6 +59,17 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         if query.data == 'check_balance':
             await query.edit_message_text(text="Your balance is $100.")
+
+
+        if query.data == "register":
+            keyboard = [
+                [InlineKeyboardButton("Chapa", callback_data='chapa'),
+                 InlineKeyboardButton("Manual", callback_data='manual')
+                 ]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            await query.edit_message_text("Select deposit method\nNote: Don't pay more than 2% as a transaction fee for each manual deposit", reply_markup=reply_markup)
+
         elif query.data in ['10','20' '50','100']:
             print("data=",query.data)
             print("username=",query.from_user.username)
@@ -89,7 +101,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await query.edit_message_text(text="An error occurred. Please try again.")
 
 async def deposit_amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    amount = update.message.text
+    amount =  float(update.message.text)
     query = update.callback_query
     first_name =  update.message.from_user.first_name
     username = update.message.from_user.username
@@ -103,6 +115,7 @@ async def deposit_amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         reference_no = f"selam_bingo_{first_name}_{datetime.now().second}"
         response = await chapa.initialize(
             email=f"{first_name}@gmail.com",
+
             amount=amount,
             currency="ETB",
             first_name=first_name,
@@ -110,8 +123,9 @@ async def deposit_amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             tx_ref=reference_no,
             callback_url="https://selambingo.onrender.com/"
         )
-        print("response = ",response)
+     
         checkout_url = response['data']['checkout_url']
+        print("checkoout url = ",checkout_url)
         if checkout_url:
             await update.message.reply_text(
                 "Open web page",
@@ -129,6 +143,55 @@ async def deposit_amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     except Exception as e:
         logger.error(f"Error processing deposit: {e}")
         await update.message.reply_text("An error occurred. Please try again.")
+
+
+
+async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    amount =  float(update.message.text)
+    query = update.callback_query
+    first_name =  update.message.from_user.first_name
+    username = update.message.from_user.username
+
+    logger.info(f"Received deposit amount: {amount}")
+
+    try:
+        amount = float(amount)  # Convert to float
+        chapa = AsyncChapa('CHASECK_TEST-vlw3GDzGJjCYI2GU9FDfInYk1L2t4KAk')
+        print("date time = ",datetime.now())
+        reference_no = f"selam_bingo_{first_name}_{datetime.now().second}"
+        response = await chapa.initialize(
+            email=f"{first_name}@gmail.com",
+
+            amount=amount,
+            currency="ETB",
+            first_name=first_name,
+            last_name=username,
+            tx_ref=reference_no,
+            callback_url="https://selambingo.onrender.com/"
+        )
+     
+        checkout_url = response['data']['checkout_url']
+        print("checkoout url = ",checkout_url)
+        if checkout_url:
+            await update.message.reply_text(
+                "Open web page",
+                reply_markup=ReplyKeyboardMarkup.from_button(
+                    KeyboardButton(
+                        text="Open Chapa!",
+                        web_app=WebAppInfo(url=checkout_url),
+                    )
+                ),
+            )
+     
+        return ConversationHandler.END  # End the conversation
+    except ValueError:
+        await update.message.reply_text("Please enter a valid number.")
+    except Exception as e:
+        logger.error(f"Error processing deposit: {e}")
+        await update.message.reply_text("An error occurred. Please try again.")
+
+
+
 
 def main() -> None:
     application = (
