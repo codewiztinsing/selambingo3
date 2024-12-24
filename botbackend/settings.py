7b25fdd4ade65b5f0c06eb34f749f64d42bc30f4
Tinsae Alako <tinsae.alako@kegeberewtech.com>
2024-12-24 12:07:40 +0300

    das

--- botbackend/settings.py
+++ botbackend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432',
     }
